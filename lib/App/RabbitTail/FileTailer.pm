a209946814121e580fcce64498bbe04d7f8435a9
t0m <bobtfish@bobtfish.net>
2010-03-01 20:41:47 +0000

    Stuff

--- lib/App/RabbitTail/FileTailer.pm
+++ lib/App/RabbitTail/FileTailer.pm
@@ -12,11 +12,53 @@ has fn => (
     coerce => 1,
 );
 
+has fh => (
+    is => 'ro',
+    lazy => 1,
+    default => sub { shift->fn->openr },
+);
+
 has cb => (
     isa => CodeRef,
     is => 'ro',
     required => 2,
 );
+
+use AnyEvent::Handle;
+
+sub tail {
+    my $self = shift;
+    my $hdl = AnyEvent::Handle->new(
+        fh => $self->fh,
+        on_error => sub {
+            my ($hdl, $fatal, $msg) = @_;
+            warn "got error $msg\n";
+            $hdl->destroy;
+        },
+        on_eof => sub {
+            my ($hdl) = @_;
+            warn("Got eof, waiting for readability");
+            $hdl->destroy;
+            my $w; $w = AnyEvent->io(
+                fh => $self->fh,
+                poll => 'r',
+                cb => sub {
+                    warn("Is readable, tailing");
+                    undef $w;
+                    $self->tail;
+                },
+            );
+        },
+    );
+
+    my $reader; $reader = sub {
+        my ($hdl, $line) = @_;
+        warn "got line <$line>\n";
+        $self->cb->($self, $line);
+        $self->push_read( line => $reader );
+    };
+    $hdl->push_read( line => $reader );
+}
 
 __PACKAGE__->meta->make_immutable;
 
