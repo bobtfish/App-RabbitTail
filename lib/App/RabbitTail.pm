18f30315d463da06f1849375f713143449afa92b
Tomas Doran <bobtfish@bobtfish.net>
2010-03-01 23:18:59 +0000

    Actually works

--- lib/App/RabbitTail.pm
+++ lib/App/RabbitTail.pm
@@ -1,9 +1,25 @@
 package App::RabbitTail;
 use Moose;
+use App::RabbitTail::FileTailer;
+use AnyEvent;
+use Coro;
 use namespace::autoclean;
 
 our $VERSION = '0.000_01';
 $VERSION = eval $VERSION;
+
+with 'MooseX::Getopt';
+
+sub run {
+    my $cv = AnyEvent->condvar;
+    my $ft = App::RabbitTail::FileTailer->new(
+        cb => sub { warn("LINE"); },
+        fn => '/Users/t0m/code/git/App-RabbitTail/test',
+    );
+    my $fh = $ft->tail();
+    warn("Did tail");
+    $cv->recv;
+}
 
 __PACKAGE__->meta->make_immutable;
 __END__
--- lib/App/RabbitTail/FileTailer.pm
+++ lib/App/RabbitTail/FileTailer.pm
@@ -1,8 +1,9 @@
 package App::RabbitTail::FileTailer;
 use Moose;
 use AnyEvent;
-use MooseX::Types::Moose qw/CodeRef/;
+use MooseX::Types::Moose qw/CodeRef Num/;
 use MooseX::Types::Path::Class qw/File/;
+use Coro::Handle;
 use namespace::autoclean;
 
 has fn => (
@@ -21,43 +22,79 @@ has fh => (
 has cb => (
     isa => CodeRef,
     is => 'ro',
-    required => 2,
+    required => 1,
 );
 
-use AnyEvent::Handle;
+has _sleep_interval => (
+    isa => Num,
+    is => 'rw',
+    default => 0,
+);
+
+has _next_backoff => (
+    isa => Num,
+    is => 'rw',
+    clearer => '_clear_next_backoff',
+    predicate => '_has_next_backoff',
+);
+
+has backoff_increment => (
+    isa => Num,
+    is => 'ro',
+    default => 0.1,
+);
+
+has max_sleep => (
+    isa => Num,
+    is => 'ro',
+    default => 10,
+);
+
+has _watcher => (
+    is => 'rw'
+);
 
 sub tail {
-    my $self = shift;
-    my $hdl = AnyEvent::Handle->new(
-        fh => $self->fh,
-        on_error => sub {
-            my ($hdl, $fatal, $msg) = @_;
-            warn "got error $msg\n";
-            $hdl->destroy;
-        },
-        on_eof => sub {
-            my ($hdl) = @_;
-            warn("Got eof, waiting for readability");
-            $hdl->destroy;
-            my $w; $w = AnyEvent->io(
-                fh => $self->fh,
-                poll => 'r',
-                cb => sub {
-                    warn("Is readable, tailing");
-                    undef $w;
-                    $self->tail;
-                },
-            );
-        },
-    );
+    my ($self) = @_;
 
-    my $reader; $reader = sub {
-        my ($hdl, $line) = @_;
-        warn "got line <$line>\n";
-        $self->cb->($self, $line);
-        $self->push_read( line => $reader );
-    };
-    $hdl->push_read( line => $reader );
+    while ($self->_read_one_line) {}
+    $self->_sleep_till_lines
+}
+
+sub _sleep_till_lines {
+    my ($self) = @_;
+    $self->_watcher(AnyEvent->timer(
+        after => $self->_sleep_interval,
+        cb => sub {
+            if ( !$self->_read_one_line ) {
+                if (!$self->_has_next_backoff) {
+                    $self->_next_backoff($self->backoff_increment);
+                }
+                $self->_sleep_interval($self->_sleep_interval + $self->_next_backoff);
+                if ($self->_sleep_interval > $self->max_sleep) {
+                    $self->_sleep_interval($self->max_sleep);
+                    $self->_next_backoff(0);
+                }
+                elsif ($self->_sleep_interval < $self->max_sleep) {
+                    $self->_next_backoff( $self->_next_backoff * 2 );
+                }
+                $self->_sleep_till_lines;
+            }
+            else {
+                $self->tail;
+            }
+        },
+    ));
+}
+
+sub _read_one_line {
+    my $self = shift;
+    my $line = unblock($self->fh)->readline;
+    return if !defined $line;
+    $self->_sleep_interval(0);
+    $self->_clear_next_backoff;
+    $self->cb->($line);
+    return $line;
 }
 
 __PACKAGE__->meta->make_immutable;
